cbc73e99824c68cacff33983a45c6d0319a1bca1
js012108 <js012108@gmail.com>
2024-03-12 18:20:22 -0500

    Clase 1

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo.... Hoy es martes");
+    System.out.println("Hola Mundo.... Hoy es 12 de marzo");
+  }
+}
---
+++ hola.py
@@ -0,0 +1,3 @@
+print('Hola Mundo')
+print('Hola Mundo..... hoy es martes')
+print('Hola Mundo...... hoy es 12 de marzo')
